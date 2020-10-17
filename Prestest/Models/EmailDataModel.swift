a45aa98b78e9baaf2ce08f8194a8cb8d4677818a
bosta <bosta.ginting@gmail.com>
2020-10-17 13:17:05 +0700

    first commit

---
+++ Prestest/Models/EmailDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  EmailDataModel.swift
+//  Prestest
+//
+//  Created by Bosta Ginting on 16/10/20.
+//
+
+import Foundation
---
+++ Prestest/Models/EmailModel.swift
@@ -0,0 +1,8 @@
+//
+//  EmailModel.swift
+//  Prestest
+//
+//  Created by Bosta Ginting on 16/10/20.
+//
+
+import Foundation
